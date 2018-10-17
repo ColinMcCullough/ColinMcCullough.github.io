9595800eccfd89a674419ba28d9a4c15908f776a
Colin McCullough <colin.n.mccullough@gmail.com>
2018-10-16 22:35:49 -0700

    Added p5.js programs page

---
+++ p5/project-2/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  var canvas = createCanvas(180, 180);
+ 
+  // Move the canvas so it’s inside our <div id="sketch-holder">.
+  canvas.parent('sketch-holder');
+
+  background(100, 0, 200);
+}
+  
+function draw() {
+    background(204);
+    ellipse(50, 50, 80, 80);
+    }
+  
+
+
